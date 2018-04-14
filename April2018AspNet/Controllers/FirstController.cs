b4535750761e9fe25bfced619b2d81202f8e847e
Usman Nazir <G_4_Genious@hotmail.com>
2018-04-14 15:22:21 +0500

    test

---
+++ April2018AspNet/Controllers/FirstController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace April2018AspNet.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
+
+    }
+}
